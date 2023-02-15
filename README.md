c384ccf3d252a8093af2fc862fc98ea1a7238c54
nathaliefc1 <nathaliefc89@gmail.com>
2023-02-15 18:00:13 +0100

    adding ErrorBoundry and text in Readme and deployed link

--- README.md
+++ README.md
@@ -1,8 +1,13 @@
 # ROBOFRIENDS
+
+Check here: https://nathaliefc1.github.io/robofriends/
+
 Introducing ROBOFRIENDS an exiting web built with Javascript and React!
 
 Usign a fun robo-themed API, robofriends generates and organized unique cards for each individual robot completed with their contact information. 
 
+![](https://media.giphy.com/media/SDUun9NqZiiPanKRaK/giphy.gif)
+
 API: https://robohash.org/
 
-Thank you!
+Thank you! :)
--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,20 +1,24 @@
 import React from "react";
 import Card from "./Card";
 
-const CardList = ({robots}) => {
-    return(
-        <div>
-            {robots.map((user, i) => {
-        return (
-            <Card 
-                key={i} 
-                id= {robots[i].id} 
-                name= {robots[i].name} 
-                email={robots[i].email}
-             />
-        );
-    })}
-        </div>
-    )
-}
+const CardList = ({ robots }) => {
+  if (false) {
+    throw new Error("NOOOO");
+  } else {
+    return (
+      <div>
+        {robots.map((user, i) => {
+          return (
+            <Card
+              key={i}
+              id={robots[i].id}
+              name={robots[i].name}
+              email={robots[i].email}
+            />
+          );
+        })}
+      </div>
+    );
+  }
+};
 export default CardList;
