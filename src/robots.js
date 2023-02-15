89ecd7ad22cb5afc237bfac9537cf48cedb7aa3b
nathaliefc1 <nathaliefc89@gmail.com>
2023-02-15 17:10:33 +0100

    robofriends project

---
+++ src/robots.js
@@ -0,0 +1,33 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Lila',
+        username: 'Beep-lila',
+        email: 'lilalila@mail.com'
+    },{
+        id: 2,
+        name: 'Uma',
+        username: 'UmaSpace',
+        email: 'umaspace@mail.com'
+    }, {
+        id: 3,
+        name: 'Francis',
+        username: 'Ponjoso',
+        email: 'ponjoso@mail.com'
+    },{
+        id: 4,
+        name: 'Vivi',
+        username: 'Mami',
+        email: 'mami@mail.com'
+    },{
+        id:5,
+        name: 'Natu',
+        username: 'Ponjosa',
+        email: 'yo@mail.com'
+    },{
+        id:6,
+        name: 'Adriz',
+        username: 'UmasMom',
+        email: 'umamommy@mail.com'
+    }
+]
